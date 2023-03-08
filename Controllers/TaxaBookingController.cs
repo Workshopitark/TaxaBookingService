edb314e490e1ba9a5235034b92f3c939ce108d01
daddel <eaaddje@students.eaaa.dk>
2023-03-08 13:53:51 +0100

    idk

--- Controllers/TaxaBookingController.cs
+++ Controllers/TaxaBookingController.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TaxaBookingService;
 using System.Xml.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using System.Threading.Channels;
 
 namespace TaxaBookingService;
+
+
+
 
 [ApiController]
 [Route("[controller]")]
@@ -15,34 +21,48 @@ public class TaxaBookingController : ControllerBase
 
 
 
-    private readonly ILogger<TaxaBookingController> _logger;
 
+    private readonly ILogger<TaxaBookingController> _logger;
+    private readonly IModel _channel;
+
+
+    // constructor
     public TaxaBookingController(ILogger<TaxaBookingController> logger, IConfiguration configuration)
     {
         _logger = logger;
-        // _imagePath = configuration["ImagePath"] ?? String.Empty;
+
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        _channel = connection.CreateModel();
+
     }
 
 
+
+
+
+    // post metode
     [HttpPost("upload"), DisableRequestSizeLimit]
-    public IActionResult UploadBooking()
+    public async Task<IActionResult> Post([FromBody] TaxaBooking taxaBooking)
     {
         _logger.LogInformation("funktion ramt");
 
+        _channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+        const string message = "Hello World daniel!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        _channel.BasicPublish(exchange: string.Empty,
+                             routingKey: "hello",
+                             basicProperties: null,
+                             body: body);
+
 
     }
 
 
-
-    private static List<TaxaBooking> _customers = new List<TaxaBooking>() {
-
-        new TaxaBooking()
-        {
-            Kundenavn = "Peter Petersen",
-            Starttidspunkt = DateTime.Now,
-            Startsted = "Nydamsvej 8",
-            Endested = "Nydamsvej 9",
-        }
-
-    };
 }
